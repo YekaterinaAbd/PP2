0deb803007c139ff481b7b1a6acc6c69fcf82a13
Abdullina Yekaterina <yekatellina@gmail.com>
2019-02-17 22:25:31 +0600

    added 1.2, upd 2.1

---
+++ Week3/Task1_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Task1_2
+{
+    class Program
+    {
+
+        static void Draw(FileSystemInfo[] Content, int selectedItem)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            for (int i = 0; i < Content.Length; i++)
+            {
+                for (int j = i + 1; j < Content.Length; j++)
+                {
+
+                    if (Content[j].GetType() == typeof(DirectoryInfo) && Content[i].GetType() == typeof(FileInfo))
+                    {
+                        var t = Content[j];
+                        Content[j] = Content[i];
+                        Content[i] = t;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < Content.Length; i++)
+            {
+                if (i == selectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                }
+                else Console.BackgroundColor = ConsoleColor.Black;
+
+                if (Content[i].GetType() == typeof(DirectoryInfo))
+                {
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }
+                else Console.ForegroundColor = ConsoleColor.Gray;
+
+                Console.WriteLine(i + 1 + ". " + Content[i].Name);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\ и\Documents\PPP2");
+            FileSystemInfo[] Content = dir.GetFileSystemInfos();
+            int selectedItem = 0;
+            Draw(Content, selectedItem);
+        }
+    }
+}
+
+
--- Week3/Task2_1/Program.cs
+++ Week3/Task2_1/Program.cs
@@ -3,35 +3,10 @@ using System.IO;
 
 namespace Task2_1
 {
-    class Level
+    class Program
     {
-        //private FileSystemInfo[] content;
-        public FileSystemInfo[] Content
-        {
-            get; //{return content}
-            set; //{content = value}
-        }
-        public int selectedItem;
 
-        public void Up()
-        {
-            if (selectedItem <= 0)
-            {
-                selectedItem = Content.Length - 1;
-            }
-            else selectedItem--;
-        }
-
-        public void Down()
-        {
-            if (selectedItem >= Content.Length-1)
-            {
-                selectedItem = 0;
-            }
-            else selectedItem++;
-        }
-
-        public void Draw()
+        static void Draw(FileSystemInfo[] Content, int selectedItem)
         {
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
@@ -69,86 +44,91 @@ namespace Task2_1
                 Console.WriteLine(i + 1 + ". " + Content[i].Name);
             }
         }
-    }
-    enum Farmode
-    {
-        DirectiryView,
-        FileView
-    }
+
+        enum Farmode
+        {
+            DirectiryView,
+            FileView
+        }
 
 
-    class Program
-    {
         static void Main(string[] args)
         {
             DirectoryInfo dir = new DirectoryInfo(@"C:\Users\ и\Documents\PPP2");
-            Level l = new Level
-            {
-                Content = dir.GetFileSystemInfos(),
-                selectedItem = 0
-            };
+            FileSystemInfo[] Content = dir.GetFileSystemInfos();
+            int selectedItem = 0;
+
+
             Farmode farmode = Farmode.DirectiryView;
+
             bool escape = false;
             while (!escape)
             {
                 if (farmode == Farmode.DirectiryView)
-                    l.Draw();
+
+                    Draw(Content, selectedItem);
 
                 ConsoleKeyInfo consoleKey = Console.ReadKey();
 
-                if (consoleKey.Key == ConsoleKey.DownArrow)
-                {
-                    l.Down();
-                }
-
                 if (consoleKey.Key == ConsoleKey.UpArrow)
                 {
-                    l.Up();
+                    if (selectedItem <= 0)
+                    {
+                        selectedItem = Content.Length - 1;
+                    }
+                    else selectedItem--;
+                }
+
+                if (consoleKey.Key == ConsoleKey.DownArrow)
+                {
+                    if (selectedItem >= Content.Length - 1)
+                    {
+                        selectedItem = 0;
+                    }
+                    else selectedItem++;
                 }
 
                 if (consoleKey.Key == ConsoleKey.Delete)
                 {
-
-
-                    FileSystemInfo fsi = l.Content[l.selectedItem];
+                    FileSystemInfo fsi = Content[selectedItem];
                     if (fsi.GetType() == typeof(DirectoryInfo))
                     {
                         var folder = fsi as DirectoryInfo;
                         Directory.Delete(folder.FullName, true);
-                        l.Content = folder.Parent.GetFileSystemInfos();
+                        Content = folder.Parent.GetFileSystemInfos();
 
                     }
                     else if (fsi.GetType() == typeof(FileInfo))
                     {
                         var file = fsi as FileInfo;
                         File.Delete(fsi.FullName);
-                        l.Content = file.Directory.GetFileSystemInfos();
+                        Content = file.Directory.GetFileSystemInfos();
                     }
                 }
 
                 if (consoleKey.Key == ConsoleKey.F2)
                 {
                     Console.Write("New name: ");
-                    FileSystemInfo fsi2 = l.Content[l.selectedItem];
+                    FileSystemInfo fsi2 = Content[selectedItem];
                     if (fsi2.GetType() == typeof(DirectoryInfo))
                     {
                         var folder2 = fsi2 as DirectoryInfo;
                         string path = Path.Combine(folder2.Parent.FullName, Console.ReadLine());
                         Directory.Move(folder2.FullName, path);
-                        l.Content = folder2.Parent.GetFileSystemInfos();
+                        Content = folder2.Parent.GetFileSystemInfos();
                     }
                     else
                     {
                         var file2 = fsi2 as FileInfo;
                         string path2 = Path.Combine(file2.Directory.FullName, Console.ReadLine());
                         File.Move(file2.FullName, path2);
-                        l.Content = file2.Directory.GetFileSystemInfos();
+                        Content = file2.Directory.GetFileSystemInfos();
                     }
                 }
 
                 if (consoleKey.Key == ConsoleKey.Enter)
                 {
-                    FileSystemInfo fsi3 = l.Content[l.selectedItem];
+                    FileSystemInfo fsi3 = Content[selectedItem];
                     if (fsi3.GetType() == typeof(FileInfo))
                     {
                         farmode = Farmode.FileView;
@@ -165,28 +145,39 @@ namespace Task2_1
                     if (fsi3.GetType() == typeof(DirectoryInfo))
                     {
                         var folder3 = fsi3 as DirectoryInfo;
-                        l.Content = folder3.GetFileSystemInfos();
-                        l.selectedItem = 0;
+
+                      //if (folder3.GetFileSystemInfos().Length == 0)
+                      //{ }
+                      //else
+                      //{
+                        Content = folder3.GetFileSystemInfos();
+                        selectedItem = 0;
+                      //}
                     }
                 }
 
                 if (consoleKey.Key == ConsoleKey.Backspace)
                 {
-                    FileSystemInfo fsi4 = l.Content[l.selectedItem];
+                    FileSystemInfo fsi4 = Content[selectedItem];
+
+                    if (fsi4 == null)
+                    {
+                        //????
+                    }
+
                     if (fsi4.GetType() == typeof(DirectoryInfo))
                     {
                         DirectoryInfo folder4 = fsi4 as DirectoryInfo;
-                        l.Content = folder4.Parent.Parent.GetFileSystemInfos();
-                        l.selectedItem = 0;
-
-
+                        Content = folder4.Parent.Parent.GetFileSystemInfos();
                     }
+
                     if (fsi4.GetType() == typeof(FileInfo))
                     {
                         var file4 = fsi4 as FileInfo;
                         Console.BackgroundColor = ConsoleColor.Black;
                         Console.ForegroundColor = ConsoleColor.White;
                         farmode = Farmode.DirectiryView;
+                        Content = file4.Directory.GetFileSystemInfos();
 
                     }
                 }
@@ -196,3 +187,13 @@ namespace Task2_1
         }
     }
 }
+
+/*works:
+ * Delete 
+ * F2 
+ * UppArrow 
+ * DownArrow 
+ * Enter 
+ */
+
+// Backspace works only if i don't open empty folders
