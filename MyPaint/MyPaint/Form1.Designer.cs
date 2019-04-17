b44aee6cba7241007d064cfd8d444b31eba47342
Abdullina Yekaterina <yekatellina@gmail.com>
2019-04-17 19:26:23 +0600

    paint

---
+++ MyPaint/MyPaint/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace MyPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pen = new System.Windows.Forms.Button();
+            this.rectangle = new System.Windows.Forms.Button();
+            this.ellipse = new System.Windows.Forms.Button();
+            this.line = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.rubber = new System.Windows.Forms.Button();
+            this.triangle = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.palette = new System.Windows.Forms.Button();
+            this.righttriangle = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(138, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1126, 573);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // pen
+            // 
+            this.pen.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pen.Image = ((System.Drawing.Image)(resources.GetObject("pen.Image")));
+            this.pen.Location = new System.Drawing.Point(6, 25);
+            this.pen.Name = "pen";
+            this.pen.Size = new System.Drawing.Size(54, 58);
+            this.pen.TabIndex = 1;
+            this.pen.UseVisualStyleBackColor = false;
+            this.pen.Click += new System.EventHandler(this.pen_Click);
+            // 
+            // rectangle
+            // 
+            this.rectangle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rectangle.Location = new System.Drawing.Point(12, 336);
+            this.rectangle.Name = "rectangle";
+            this.rectangle.Size = new System.Drawing.Size(91, 50);
+            this.rectangle.TabIndex = 2;
+            this.rectangle.Text = "rectangle";
+            this.rectangle.UseVisualStyleBackColor = false;
+            this.rectangle.Click += new System.EventHandler(this.rectangle_Click);
+            // 
+            // ellipse
+            // 
+            this.ellipse.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ellipse.Location = new System.Drawing.Point(12, 392);
+            this.ellipse.Name = "ellipse";
+            this.ellipse.Size = new System.Drawing.Size(91, 50);
+            this.ellipse.TabIndex = 3;
+            this.ellipse.Text = "ellipse";
+            this.ellipse.UseVisualStyleBackColor = false;
+            this.ellipse.Click += new System.EventHandler(this.ellipse_Click);
+            // 
+            // line
+            // 
+            this.line.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.line.Location = new System.Drawing.Point(12, 283);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(91, 50);
+            this.line.TabIndex = 4;
+            this.line.Text = "line";
+            this.line.UseVisualStyleBackColor = false;
+            this.line.Click += new System.EventHandler(this.line_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.colorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1282, 33);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(67, 29);
+            this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // rubber
+            // 
+            this.rubber.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rubber.Image = ((System.Drawing.Image)(resources.GetObject("rubber.Image")));
+            this.rubber.Location = new System.Drawing.Point(60, 25);
+            this.rubber.Name = "rubber";
+            this.rubber.Size = new System.Drawing.Size(54, 58);
+            this.rubber.TabIndex = 8;
+            this.rubber.UseVisualStyleBackColor = false;
+            this.rubber.Click += new System.EventHandler(this.rubber_Click);
+            // 
+            // triangle
+            // 
+            this.triangle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.triangle.Location = new System.Drawing.Point(12, 448);
+            this.triangle.Name = "triangle";
+            this.triangle.Size = new System.Drawing.Size(91, 50);
+            this.triangle.TabIndex = 9;
+            this.triangle.Text = "triangle";
+            this.triangle.UseVisualStyleBackColor = false;
+            this.triangle.Click += new System.EventHandler(this.triangle_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 151);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(108, 26);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // palette
+            // 
+            this.palette.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.palette.Image = ((System.Drawing.Image)(resources.GetObject("palette.Image")));
+            this.palette.Location = new System.Drawing.Point(6, 86);
+            this.palette.Name = "palette";
+            this.palette.Size = new System.Drawing.Size(54, 59);
+            this.palette.TabIndex = 11;
+            this.palette.UseVisualStyleBackColor = false;
+            this.palette.Click += new System.EventHandler(this.palette_Click);
+            // 
+            // righttriangle
+            // 
+            this.righttriangle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.righttriangle.Location = new System.Drawing.Point(12, 504);
+            this.righttriangle.Name = "righttriangle";
+            this.righttriangle.Size = new System.Drawing.Size(91, 50);
+            this.righttriangle.TabIndex = 12;
+            this.righttriangle.Text = "right trian";
+            this.righttriangle.UseVisualStyleBackColor = false;
+            this.righttriangle.Click += new System.EventHandler(this.righttriangle_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox1.Controls.Add(this.pen);
+            this.groupBox1.Controls.Add(this.rubber);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.palette);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(120, 183);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1282, 641);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.righttriangle);
+            this.Controls.Add(this.triangle);
+            this.Controls.Add(this.ellipse);
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.rectangle);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button pen;
+        private System.Windows.Forms.Button rectangle;
+        private System.Windows.Forms.Button ellipse;
+        private System.Windows.Forms.Button line;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button rubber;
+        private System.Windows.Forms.Button triangle;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button palette;
+        private System.Windows.Forms.Button righttriangle;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ MyPaint/MyPaint/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPaint
+{
+    public enum Tool
+    {
+        PEN, LINE, RECTANGLE, ELLIPSE, TRIANGLE,
+        RIGHTTRIANGLE
+    };
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        Graphics graphics;
+        Point firstPoint, secondPoint;
+        Pen pencil = new Pen(Color.Black, 2);
+        Tool tool = Tool.PEN;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.White);
+            pictureBox1.Image = bitmap;
+            pencil.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pencil.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        private void rectangle_Click(object sender, EventArgs e)
+        {
+            tool = Tool.RECTANGLE;
+        }
+
+        private void line_Click(object sender, EventArgs e)
+        {
+            tool = Tool.LINE;
+        }
+
+        private void ellipse_Click(object sender, EventArgs e)
+        {
+            tool = Tool.ELLIPSE;
+        }
+
+        private void pen_Click(object sender, EventArgs e)
+        {
+            tool = Tool.PEN;
+        }
+        private void triangle_Click(object sender, EventArgs e)
+        {
+            tool = Tool.TRIANGLE;
+        }
+        private void righttriangle_Click(object sender, EventArgs e)
+        {
+            tool = Tool.RIGHTTRIANGLE;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            firstPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (tool)
+            {
+                case Tool.PEN:
+                    break;
+                case Tool.LINE:
+                    secondPoint = e.Location;
+                    graphics.DrawLine(pencil, firstPoint, secondPoint); //drawing on bitmap with saving picture
+                    pictureBox1.Refresh();
+                    break;
+                case Tool.RECTANGLE:
+                    graphics.DrawRectangle(pencil, GetRectangle(firstPoint, secondPoint));
+                    break;
+                case Tool.ELLIPSE:
+                    graphics.DrawEllipse(pencil, GetRectangle(firstPoint, secondPoint));
+                    break;
+                case Tool.TRIANGLE:
+                    graphics.DrawPolygon(pencil, GetTriangle(firstPoint, secondPoint));
+                    break;
+                case Tool.RIGHTTRIANGLE:
+                    graphics.DrawPolygon(pencil, GetrightTriangle(firstPoint, secondPoint));
+                    break;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            switch (tool)
+            {
+                case Tool.PEN:
+                    break;
+                case Tool.LINE:
+                    e.Graphics.DrawLine(pencil, firstPoint, secondPoint);
+                    //drawing on picturebox to make the line initially visible, then it disappears
+                    break;
+                case Tool.RECTANGLE:
+                    e.Graphics.DrawRectangle(pencil, GetRectangle(firstPoint, secondPoint));
+                    break;
+                case Tool.ELLIPSE:
+                    e.Graphics.DrawEllipse(pencil, GetRectangle(firstPoint, secondPoint));
+                    break;
+                case Tool.TRIANGLE:
+                    e.Graphics.DrawPolygon(pencil, GetTriangle(firstPoint, secondPoint));
+                    break;
+                case Tool.RIGHTTRIANGLE:
+                    e.Graphics.DrawPolygon(pencil, GetrightTriangle(firstPoint, secondPoint));
+                    break;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                secondPoint = e.Location;
+
+                switch (tool)
+                {
+                    case Tool.PEN:
+                        graphics.DrawLine(pencil, firstPoint, secondPoint);
+                        firstPoint = secondPoint;
+                        break;
+                    case Tool.LINE:
+                        break;
+                    case Tool.RECTANGLE:
+                        break;
+                    case Tool.ELLIPSE:
+                        break;
+                }
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pencil.Color = colorDialog1.Color;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bitmap.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bitmap = new Bitmap(openFileDialog1.FileName);
+                graphics = Graphics.FromImage(bitmap);
+                pictureBox1.Image = bitmap;
+            }
+        }
+
+        private void rubber_Click(object sender, EventArgs e)
+        {
+            tool = Tool.PEN;
+            pencil.Color = Color.White;
+        }
+
+
+        public Rectangle GetRectangle(Point first, Point second)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.X = Math.Min(first.X, second.X);
+            rectangle.Y = Math.Min(first.Y, second.Y);
+
+            rectangle.Width = Math.Abs(second.X - first.X);
+            rectangle.Height = Math.Abs(second.Y - first.Y);
+
+            return rectangle;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+           
+            pencil.Width = decimal.ToInt32(numericUpDown1.Value);
+        }
+
+        private void palette_Click(object sender, EventArgs e)
+        {
+            pencil.Color = Color.FromArgb(bitmap.GetPixel(firstPoint.X, firstPoint.Y).ToArgb());
+            
+        }
+
+        public Point[] GetTriangle(Point first, Point second)
+        {
+            Point[] triangle =
+            {
+                new Point(first.X, first.Y),
+                new Point(second.X, first.Y),
+                new Point((second.X + firstPoint.X)/2, second.Y)
+            };
+
+            return triangle;
+        }
+
+
+        public Point[] GetrightTriangle(Point first, Point second)
+        {
+            Point[] triangle =
+            {
+                new Point(first.X, first.Y),
+                new Point(second.X, first.Y),
+                new Point(first.X, second.Y)
+            };
+            return triangle;
+        }
+    }
+}
